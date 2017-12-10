eeb25e7eed3fe0ecaa2f57e1e52e7f242edb54fa
Create Unity Project <ryohei@R-NakanonoMacBook-Pro.local>
2017-12-10 21:38:27 +0900

    ・Mainシーン作成 ・EnemySpawnerScript作成 ・Red,Blue,GreenのEnemy作成

---
+++ Assets/Scripts/EnemySpawnerScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnerScript : MonoBehaviour {
+
+	[SerializeField] GameObject[] enemyType = new GameObject[3];//Unity上からアタッチ！
+	//0→Red
+	//1→Blue
+	//2→Green
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
